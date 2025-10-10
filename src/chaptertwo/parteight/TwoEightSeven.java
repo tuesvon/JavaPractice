4fd9ca65aae91d3fac3692ad8999fa28c9de7843
kanaevde <dkanaev@phoenixit.ru>
2025-10-10 16:14:03 +0300

    2.8 Тесты

--- src/chaptertwo/parteight/TwoEightSeven.java
+++ src/chaptertwo/parteight/TwoEightSeven.java
@@ -1,4 +1,15 @@
 package chaptertwo.parteight;
 
-public class TwoEightSeven {
+import java.util.Scanner;
+
+class TwoEightSeven {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+        double x1 = (-1 * b) / a;
+        double x2 = c / a;
+        System.out.print(x1 + " " + x2);
+    }
 }
