6165e606c37ae736938507adc043916c6ddc0e8b
kanaevde <dkanaev@phoenixit.ru>
2025-10-10 15:38:58 +0300

    2.8 Тесты

--- src/chaptertwo/parteight/TwoEightOne.java
+++ src/chaptertwo/parteight/TwoEightOne.java
@@ -1,4 +1,10 @@
 package chaptertwo.parteight;
 
 public class TwoEightOne {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 7;
+        int sumNum = x + y;
+        System.out.print(sumNum);
+    }
 }
