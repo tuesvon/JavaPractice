e5986fe02b584af60aa9d15310b1d703c5f66e85
kanaevde <dkanaev@phoenixit.ru>
2025-10-10 21:44:20 +0300

    2.8 Тесты

--- src/chaptertwo/parteight/TwoEightEight.java
+++ src/chaptertwo/parteight/TwoEightEight.java
@@ -1,4 +1,12 @@
 package chaptertwo.parteight;
 
-public class TwoEightEight {
+import java.util.Scanner;
+
+class TwoEightEight {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int z = a / 400 - a / 100 + a / 4 ;
+        System.out.print(z);
+    }
 }
--- src/chaptertwo/parteight/TwoEightNine.java
+++ src/chaptertwo/parteight/TwoEightNine.java
@@ -1,4 +1,15 @@
 package chaptertwo.parteight;
 
-public class TwoEightNine {
+import java.util.Scanner;
+
+class TwoEightNine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int sum = x + y ;
+        int diff = x - y;
+        System.out.println("Сложение: " + x + " + " + y + " = " + sum);
+        System.out.print("Вычитание: " + x + " - " + y + " = " + diff);
+    }
 }
