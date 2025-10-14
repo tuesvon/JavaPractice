3e9d94d27c24e86fcd0915e2abd6092d420b560e
kanaevde <dkanaev@phoenixit.ru>
2025-10-14 11:59:11 +0300

    3.1

---
+++ src/chapterthree/partone/ThreeOneTwo.java
@@ -0,0 +1,10 @@
+package chapterthree.partone;
+
+class ThreeOneTwo {
+    public static void main(String[] args) {
+        int x = 5;
+        if (x == 5) {
+            System.out.println("YES");
+        }
+    }
+}
